40a1a0c6d52f3e6922ff1d50e70ec681fc7cf814
jake zilch <jzilch280@gmail.com>
2017-04-16 21:47:19 -0500

    Added all 3 templates with website working without  redirects

--- main.py
+++ main.py
@@ -1,25 +1,65 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+import os
 import webapp2
+import jinja2
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class ViewPostHandler(webapp2.RequestHandler):
+    def get(self, id):
+        post = Model.get_by_id
+        self.response.write(post)
+
+class Blog(db.Model):
+    title = db.StringProperty(required = True)
+    blog = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(Handler):
+    def render_base(self):
+        blogs = db.GqlQuery("SELECT * FROM Blog "
+                            "ORDER BY created DESC ")
+        self.render("frontpage.html", blogs=blogs)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_base()
+
+class NewPost(Handler):
+    def render_front(self, title="", blog="", error=""):
+        self.render("newpost.html", title=title, blog=blog, error=error)
+
+    def get(self):
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        blog = self.request.get("blog")
+
+        if title and blog:
+            a = Blog(title = title, blog = blog)
+            a.put()
+            self.redirect("/blog")
+        else:
+            error = "We need both the title and blogpost!"
+            self.render_front(title, blog, error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage),
+    ('/blog', MainPage),
+    ('/newpost', NewPost),
+    webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
 ], debug=True)
